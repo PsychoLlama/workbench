6c51a3b08ae2f68f20868f35818638b7f64b9418
Jesse Gibson <JesseTheGibson@gmail.com>
2025-04-20 15:54:42 -0400

    Fix emoji display on Chromium
    It seems Chromium doesn't implement the standard necessary to display
    color fonts that fontsource expects.

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,4 +1,6 @@
 import '@fontsource-variable/inter';
-import '@fontsource/noto-color-emoji';
+
+// Using fontsource fork. See: https://github.com/fontsource/fontsource/issues/588
+import '@infolektuell/noto-color-emoji';
 
 export { Page as wrapPageElement } from './src/components/Page';
